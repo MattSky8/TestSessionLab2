3e875f3afc96109c3aedddd90eb41543655ecd8f
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-03-28 11:52:39 -0500

    Update views.py
    Reformatted again

--- StuffTracker/views.py
+++ StuffTracker/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Stuff, MyUser
+
+
 # Create your views here.
 
 class Home(View):
-    def get(self,request):
-        return render(request,"home.html",{})
-    def post(self,request):
+    def get(self, request):
+        return render(request, "home.html", {})
+
+    def post(self, request):
         noSuchUser = False
         badPassword = False
         try:
@@ -15,27 +18,28 @@ class Home(View):
         except:
             noSuchUser = True
         if noSuchUser:
-            m = MyUser(name=request.POST['name'], password = request.POST['password'])
+            m = MyUser(name=request.POST['name'], password=request.POST['password'])
             m.save()
             request.session["name"] = m.name
             return redirect("/things/")
         elif badPassword:
-            return render(request,"home.html",{"message":"bad password"})
+            return render(request, "home.html", {"message": "bad password"})
         else:
             request.session["name"] = m.name
             return redirect("/things/")
 
 
 class Things(View):
-    def get(self,request):
+    def get(self, request):
         m = request.session["name"]
-        things = list(map(str,Stuff.objects.filter(owner__name=m)))
-        return render(request, "things.html", {"name":m, "things":things})
-    def post(self,request):
+        things = list(map(str, Stuff.objects.filter(owner__name=m)))
+        return render(request, "things.html", {"name": m, "things": things})
+
+    def post(self, request):
         m = request.session["name"]
-        s = request.POST.get('stuff','')
+        s = request.POST.get('stuff', '')
         if s != '':
-            newThing = Stuff(name=s,owner=MyUser.objects.get(name=m))
+            newThing = Stuff(name=s, owner=MyUser.objects.get(name=m))
             newThing.save()
-        things = list(map(str,Stuff.objects.filter(owner__name=m)))
-        return render(request, "things.html", {"name":m, "things":things})
+        things = list(map(str, Stuff.objects.filter(owner__name=m)))
+        return render(request, "things.html", {"name": m, "things": things})
