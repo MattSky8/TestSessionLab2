f3fb1d3f7dfaed01e339f90b42837223eaa35b16
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-03-28 12:38:07 -0500

    Update tests.py
    Wrote tests

--- StuffTracker/tests.py
+++ StuffTracker/tests.py
@@ -64,8 +64,49 @@ class LoginFail(TestCase):
     # test methods should confirm correct error message is displayed when a bad password is entered.
     # I had separate tests for no password, someone else's password
 
+    def test_incorrect_password(self):
+        randompasswords = ['ahf', 'hello', 'admin', '50d022od']
+        for user in self.thingList.keys():
+            for password in randompasswords:
+                if not user.__eq__(password):
+                    resp = self.monkey.post("/", {"name": user, "password": password}, follow=True)
+                    self.assertEqual(resp.context["message"], "bad password",
+                                     "wrong error message: expected \"bad password\".")
+
+    def test_no_password(self):
+        for user in self.thingList.keys():
+            resp = self.monkey.post("/", {"name": user, "password": ""}, follow=True)
+            self.assertEqual(resp.context["message"], "bad password",
+                             "wrong error message: expected \"bad password\".")
+
+    def test_other_password(self):
+        for user in self.thingList.keys():
+            for password in self.thingList.keys():
+                if not user.__eq__(password):
+                    resp = self.monkey.post("/", {"name": user, "password": password}, follow=True)
+                    self.assertEqual(resp.context["message"], "bad password",
+                                     "wrong error message: expected \"bad password\".")
+
 
 class AddItem(TestCase):
-    pass
+    monkey = None
+    thingList = None
+    user = None
+
+    def setUp(self):
+        # completed
+        self.monkey = Client()
+        self.thingsToAdd = ["cat", "dog", "cake"]
+        # fill test database
+        self.user = "one"
+        temp = MyUser(name=self.user, password=self.user)
+        temp.save()
+        self.monkey.post("/", {"name": self.user, "password": self.user}, follow=True)
+
     # need to create database in setup
     # confirm that after an add item form is submitted, that the new item is in the database and appears in the response webpage
+
+    def test_item_added(self):
+        for item in self.thingsToAdd:
+            resp = self.monkey.post("/things/", {"name": self.user, "stuff": item}, follow=True)
+            self.assertIn(item, resp.context["things"], "item not added to user's list")
