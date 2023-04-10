bdb5db9592e8da77bbc68245f63a340e78af40d5
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-04-10 14:12:32 -0500

    Update test_add_items.py

--- StuffTracker/tests/test_add_items.py
+++ StuffTracker/tests/test_add_items.py
@@ -3,4 +3,83 @@ from StuffTracker.models import Stuff, MyUser
 
 
 # test class for add item criterion 1:
-# ...
+# when a nonempty string is entered,
+# the item should be added to the user's list and displayed
+class AddItemSimple(TestCase):
+    monkey = None
+    users = None
+
+    def setUp(self):
+        self.monkey = Client()
+        self.users = {"user1": ["item1", "item3"], "user2": ["item2"], "user3": []}
+
+        for username in self.users.keys():
+            temp_user = MyUser(name=username, password=username)
+            temp_user.save()
+
+    def test_item_added(self):
+        for user in self.users.keys():
+            for item in self.users[user]:
+                resp = self.monkey.post("/things/", {"name": user, "stuff": item}, follow=True)
+                self.assertIn(item, resp.context["things"], "item not added to user's list")
+
+    def test_no_extra_items(self):
+        for user in self.users.keys():
+            for item in self.users[user]:
+                resp = self.monkey.post("/things/", {"name": user, "stuff": item}, follow=True)
+                for resp_item in resp.context["things"]:
+                    self.assertIn(resp_item, self.users[user], "extra item in user's list")
+
+# test class for add item criteron 2:
+# when an empty string is entered,
+# no item should be added to the user's list or displayed
+class AddItemEmptyString(TestCase):
+    monkey = None
+    users = None
+
+    def setUp(self):
+        self.monkey = Client()
+        self.users = {"user1": ["item1", "item3"], "user2": ["item2"], "user3": []}
+
+        for username in self.users.keys():
+            temp_user = MyUser(name=username, password=username)
+            temp_user.save()
+            for item in self.users[username]:
+                Stuff(name=item, owner=temp_user).save()
+
+    def test_empty_string_not_added(self):
+        for user in self.users.keys():
+            resp = self.monkey.post("/things/", {"name": user, "stuff": ""}, follow=True)
+            self.assertNotIn("", resp.context["things"], "empty string item should not be added to user's list")
+
+    def test_correct_size(self):
+        for user in self.users.keys():
+            for item in self.users[user]:
+                resp = self.monkey.post("/things/", {"name": user, "stuff": ""}, follow=True)
+                for resp_item in resp.context["things"]:
+                    self.assertIn(resp_item, self.users[user], "extra item in user's list")
+
+# test class for add item criterion 3:
+# when a duplicate item is entered,
+# the item should still be added
+class AddItemDuplicate(TestCase):
+    monkey = None
+    users = None
+
+    def setUp(self):
+        self.monkey = Client()
+        self.users = {"user1": ["item1", "item3"], "user2": ["item2"], "user3": []}
+
+        for username in self.users.keys():
+            temp_user = MyUser(name=username, password=username)
+            temp_user.save()
+            for item in self.users[username]:
+                Stuff(name=item, owner=temp_user).save()
+
+    def test_duplicate_added(self):
+        for user in self.users.keys():
+            expected_list_size = len(self.users[user])
+            for item in self.users[user]:
+                resp = self.monkey.post("/things/", {"name": user, "stuff": item}, follow=True)
+                ++expected_list_size
+                self.assertEqual(expected_list_size, len(resp.context["things"]), "duplicate item not added to list")
