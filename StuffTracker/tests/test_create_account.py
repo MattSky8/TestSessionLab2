317a9cb84d62fbac2080d777f408e00df78a33b3
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-04-10 14:53:25 -0500

    Update test_create_account.py

--- StuffTracker/tests/test_create_account.py
+++ StuffTracker/tests/test_create_account.py
@@ -1,3 +1,14 @@
 from django.test import TestCase, Client
 from StuffTracker.models import Stuff, MyUser
 
+# test class for create account criterion 1:
+# when an unused username and a password are entered
+# then that account should be created, the user should be logged in,
+# they should be redirected to the item page, and their list should be empty.
+
+
+# test class for create account criterion 2:
+# when an in-use username and nonmatching password is entered,
+# then the user should not be logged in and no changes should be made to the account.
+
+
